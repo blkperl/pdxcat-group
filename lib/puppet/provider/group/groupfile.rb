19a511e372751ae32489496758ba8f1528a3ccc4
Reid Vandewiele <marut@cat.pdx.edu>
2011-06-28 10:58:42 -0700

    Splitting gpasswd provider into groupfile
    Rather than having direct /etc/group file editing
    be an "alternate" behavior of the gpasswd provider.

--- lib/puppet/provider/group/groupfile.rb
+++ lib/puppet/provider/group/groupfile.rb
@@ -2,14 +2,13 @@ require 'puppet'
 require 'etc'
 require 'fileutils'
 
-Puppet::Type.type(:group).provide(:gpasswd) do
-  desc "Group management using groupmod and gpasswd."
+Puppet::Type.type(:group).provide(:groupfile) do
+  desc "Group management using groupmod and file editing."
 
   commands(
     :groupadd => "/usr/sbin/groupadd",
     :groupdel => "/usr/sbin/groupdel",
-    :groupmod => "/usr/sbin/groupmod",
-    :gpasswd  => "/usr/bin/gpasswd"
+    :groupmod => "/usr/sbin/groupmod"
   )
 
   has_feature :manages_members
@@ -69,8 +68,25 @@ Puppet::Type.type(:group).provide(:gpasswd) do
   end
 
   def members=(value)
-    cmd = [command(:gpasswd)]
-    cmd << '-M' << value.join(',') << @resource[:name]
-    execute(cmd)
+    begin
+      groupfile_path_tmp = '/etc/group.puppettmp'
+      groupfile_path = '/etc/group'
+      groupfile_tmp = File.open(groupfile_path_tmp, 'w')
+      groupfile = File.foreach(groupfile_path) do |line|
+        if groupline = line.match(/^#{@resource[:name]}:[x*]?:[0-9]+:/)
+          Puppet.debug "Writing members " << value.join(',') << " to " << groupfile_path_tmp
+          groupfile_tmp.puts groupline[0] + value.join(',')
+        else
+          groupfile_tmp.puts line
+        end
+      end
+      groupfile_tmp.fsync
+      groupfile_tmp.close 
+      Puppet.debug "Saving " << groupfile_path_tmp << " to " << groupfile_path
+      File.rename(groupfile_path_tmp, groupfile_path)
+    rescue Exception => e
+      FileUtils.deleteQuietly(groupfile_tmp)
+      raise Puppet::Error.new(e.message)
+    end
   end
 end
